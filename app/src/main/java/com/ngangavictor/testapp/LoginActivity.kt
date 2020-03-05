dfa0b6cd396ed75a6f31faa5fb293b2e47cb48fe
ngangavic <ngangavictor10@gmail.com>
2020-03-05 20:14:34 +0300

    register UI

--- app/src/main/java/com/ngangavictor/testapp/LoginActivity.kt
+++ app/src/main/java/com/ngangavictor/testapp/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.ngangavictor.testapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var buttonSignUp:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonSignUp=findViewById(R.id.buttonSignUp)
+
+        buttonSignUp.setOnClickListener {
+            startActivity(Intent(applicationContext,RegisterActivity::class.java))
+        }
     }
 }
