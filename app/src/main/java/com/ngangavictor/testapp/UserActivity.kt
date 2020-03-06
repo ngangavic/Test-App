d1f1f263e2236f214018dbbed2d6b64ca25f65f2
ngangavic <ngangavictor10@gmail.com>
2020-03-06 11:40:14 +0300

    user menu and logout

--- app/src/main/java/com/ngangavictor/testapp/UserActivity.kt
+++ app/src/main/java/com/ngangavictor/testapp/UserActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.DefaultRetryPolicy
@@ -66,6 +68,22 @@ class UserActivity : AppCompatActivity() {
             DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
         )
         queue.add(str)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.user_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_logout->{
+                startActivity(Intent(applicationContext,LoginActivity::class.java))
+                finish()
+                Toast.makeText(applicationContext,"Logout successful",Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
